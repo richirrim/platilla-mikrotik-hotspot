563d66bdd1af9430f8638e10feba4fcae3a522a7
richirrim <ricardoortega.dev@gmail.com>
2020-04-24 23:05:21 -0500

    Se cambiarón los taps por espacios y tambien la identacion a 2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,24 +10,24 @@ import browserSync from 'browser-sync'
 const server = browserSync.create()
 sass.compiler = require('node-sass')
 
-var paths = {
-    root: './public',
-    html: {
-      src: './dev/pug/pages/**/*.pug',
-      dest: './public/'
-    },
-    styles: {
-      src: './dev/scss/**/*.scss',
-      dest: './public/css/'
-    },
-    scripts: {
-      src: './dev/js/**/*.js',
-      dest: './public/js/'
-    },
-    images: {
-      src: './dev/images/**/*',
-      dest: './public/images/'
-    }
+const paths = {
+  root: './public',
+  html: {
+    src: './dev/pug/pages/**/*.pug',
+    dest: './public/'
+  },
+  styles: {
+    src: './dev/scss/**/*.scss',
+    dest: './public/css/'
+  },
+  scripts: {
+    src: './dev/js/**/*.js',
+    dest: './public/js/'
+  },
+  images: {
+    src: './dev/images/**/*',
+    dest: './public/images/'
+  }
 };
 
 function babelJs () {
@@ -44,15 +44,15 @@ function babelJs () {
 }
 
 function pugToHtml () {
-    return src(paths.html.src)
-      .pipe(plumber())
-      .pipe(pug({
-          pretty: true,
-          /* Agregar el "basedir" permite trabajar con rutas abosolutas o 
-            relativas dentro de archivos "".pug". */
-          basedir: './dev/pug'
-        }))
-      .pipe(dest(paths.html.dest))
+  return src(paths.html.src)
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true,
+        /* Agregar el "basedir" permite trabajar con rutas abosolutas o 
+        relativas dentro de archivos "".pug". */
+        basedir: './dev/pug'
+    }))
+    .pipe(dest(paths.html.dest))
 }
 
 function sassToCss () {
@@ -69,28 +69,28 @@ function sassToCss () {
 function optimizeImg () {
   return src(paths.images.src)
     .pipe(imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo()
-      ]))
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo()
+    ]))
     .pipe(dest(paths.images.dest))
 }
 
 function startServer () {
-    server.init({
-        server: { 
-          baseDir: paths.root 
-        }
-    })
+  server.init({
+    server: { 
+      baseDir: paths.root 
+    }
+  })
 }
 
 function watchFiles () {
-    watch(paths.html.src, pugToHtml)
-    watch(paths.styles.src, sassToCss)
-    watch(paths.scripts.src, babelJs)
-    watch(paths.html.dest).on('change', server.reload)
-    watch(paths.styles.dest).on('change', server.reload)
+  watch(paths.html.src, pugToHtml)
+  watch(paths.styles.src, sassToCss)
+  watch(paths.scripts.src, babelJs)
+  watch(paths.html.dest).on('change', server.reload)
+  watch(paths.styles.dest).on('change', server.reload)
 }
 
 exports.default = parallel(startServer, optimizeImg, series(sassToCss, pugToHtml, babelJs, watchFiles))
