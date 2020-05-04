e336ea36d5a7f5b2c7d2517323eaf16403c2bba4
richirrim <ricardoortega.dev@gmail.com>
2020-05-04 13:32:24 -0500

    Agrega la función para formatear el número telofinico

--- dev/js/script.js
+++ dev/js/script.js
@@ -12,4 +12,39 @@ const toggleMenu = function (idIconMenu, idNav) {
   })
 }
 
+const phoneNumberValidation = function () {
+  const buttonSocialEl = document.getElementById('js-btnContact')
+  let number = document.getElementById('js-btnContact').textContent
+  const isNumber = /[0-9]/g
+  const includeCharacterMore = /\x2b/g // +
+  const numberSubstring = number.substring(0,3)
+  const includeLada = numberSubstring.includes('+52') || numberSubstring.includes('52')
+
+  if (!number) return
+  if (isNumber.test(number)) {
+
+    if (!(includeLada)) {
+      number = `52 ${number}`
+      buttonSocialEl.textContent = number
+    }
+
+    if (!(includeCharacterMore.test(number))) {
+      number = `+ ${number}`
+      buttonSocialEl.textContent = number
+    }
+
+    buttonSocialEl.textContent = number
+      // Elimina todos los whitespaces.
+      .replace(/\s/g, '')
+      // Añade un espacio despues de la lada "+52".
+      .replace(/(\+52){1}/g, '$1 ')
+      // Añade un espacio despues del número "981".
+      .replace(/(981){1}/g, '$1 ')
+      // Añade un espacio despues los priemeros 3 números que están despues del "981".
+      .replace(/((981)+\s[0-9]{3}){1}/, '$1 ')
+  }
+}
+// Validar telefo, el formato ya esta
+// Validar el email y su formato
+phoneNumberValidation()
 toggleMenu('js-menu-icon', 'js-nav')
