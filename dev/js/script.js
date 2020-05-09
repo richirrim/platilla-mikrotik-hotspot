b4854176a8667fcbca654b1455f05f61d09f1e59
richirrim <ricardoortega.dev@gmail.com>
2020-05-09 17:18:00 -0500

    Refactoriza el código de la función textClamp

--- dev/js/script.js
+++ dev/js/script.js
@@ -84,26 +84,46 @@ const phoneNumberValidation = function () {
 
 
 /**
- * function: textClamp()
+ * function: addClampSingleeElement()
  * 
- * @param {string} idTextClamp 
+ * @param {string} id
  * @param {number} numberLine 
  * 
  * Limita un parrafo a las líneas que se decee mostrar (visualmente).
- *  Por defecto, muestra 5 líneas la función.
+ * Por defecto, muestra 5 líneas la función. Esta función solo modifica
+ * un elemento.
  */
-const textClamp = function (idTextClamp, numberLine) {
+const addClampSingleElement = function (id, numberLine) {
   // https://github.com/josephschmitt/Clamp.js
-  const pEl = document.getElementById(idTextClamp)
+  const pEl = document.getElementById(id)
   if (!pEl) return
+  if (typeof pEl === 'string') return
   if (!numberLine) {
     $clamp(pEl, { clamp: 5 })
     return
-  }
+  }         
   $clamp(pEl, { clamp: numberLine })
 }
+
+/**
+ * function: addClampMultipleElements()
+ * 
+ * Limita un parrafo a las líneas que se decee mostrar (visualmente).
+ * Esta función modifica más de un elemento.
+ */
+const addClampMultipleElements = function (class) {
+  const cards = Array.from(document.querySelectorAll('#js-card-service'))
+  if (!(cards.length > 0)) return
+
+  for (const card of cards) {
+    const text = card.querySelector(class)
+    if (!text) return
+    addClampSingleElement(text.id)
+  }
+}
+
+addClampMultipleElements('.card-services__text')
 // Validar sea un telefono con la estructura real, el texto ya esta formateado.
 // Validar el email y su formato
-textClamp('js-textClamp')
 phoneNumberValidation()
 toggleMenu('js-menu-icon', 'js-nav')
