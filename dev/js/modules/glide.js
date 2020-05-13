b57949b7d47825cec8bc9d19bbd35ffb2284612d
richirrim <ricardoortega.dev@gmail.com>
2020-05-13 14:01:49 -0500

    Vuelve response el glide y las card ocupan el 100% en altura

--- dev/js/modules/glide.js
+++ dev/js/modules/glide.js
@@ -9,13 +9,25 @@ export const glide = function (id) {
     options: {
       type: 'slide',
       startAt: 0,
-      perView: 3,
       focusAt: 'center',
       keyboard: true,
+      perView: 2,
       gap: 24,
       breakpoints: {
-        720: { perView: 2 },
-        480: { perView: 1 }
+        1200: {
+          perView: 3,
+          peek: 60,
+          gap: 16
+        },
+        1050: { peek: 80 },
+        900: { gap: 24 },
+        800: { gap: 32 },
+        700: { peek: 160 },
+        600: { peek: 120 },
+        540: { peek: 80 },
+        500: { peek: 90, },
+        480: { peek: 20 },
+        380: { peek: 0 }
       }
     }
   }
